168719ff6e2b85bc62c8c149b02618614865a39e
Chany <chany.vargas@upeu.edu.pe>
2025-06-22 22:49:18 -0500

    arquitectura s3

--- main.tf
+++ main.tf
@@ -1,12 +1,27 @@
-provider "aws" {
-  region = var.aws_region
+resource "aws_s3_bucket" "input_bucket" {
+  bucket = "input-bucket-${random_string.suffix.result}"
+  versioning {
+    enabled = true
+  }
 }
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = var.s3_bucket_name
-
-  tags = {
-    Name        = "MiBucket"
-    Environment = "Dev"
+resource "aws_s3_bucket" "output_bucket" {
+  bucket = "output-bucket-${random_string.suffix.result}"
+  versioning {
+    enabled = true
   }
+}
+
+resource "aws_s3_bucket_notification" "input_notification" {
+  bucket = aws_s3_bucket.input_bucket.id
+  lambda_function {
+    lambda_function_arn = var.lambda_arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_suffix       = ".csv"
+  }
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
 }
