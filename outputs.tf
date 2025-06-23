7a8970778982f09fc30611a4700435e244e9e029
Chany1906 <chany.fabrizio@gmail.com>
2025-06-23 01:48:44 -0500

    buacket name

--- outputs.tf
+++ outputs.tf
@@ -11,3 +11,7 @@ output "input_bucket_arn" {
 output "output_bucket_arn" { 
   value = aws_s3_bucket.output_bucket.arn 
 } 
+output "bucket_name" {
+  description = "Nombre del bucket de entrada"
+  value       = aws_s3_bucket.input_bucket.bucket
+}
