e54bd9d3bd107f6ebd286746c37ef79d0943ef51
Chany1906 <chany.fabrizio@gmail.com>
2025-06-23 02:12:09 -0500

    cambio

--- main.tf
+++ main.tf
@@ -24,5 +24,8 @@ resource "aws_s3_bucket_notification" "input_notification" {
 
 resource "random_string" "suffix" {
   length  = 8
+  lower   = true
+  upper   = false
   special = false
+  number  = true
 }
